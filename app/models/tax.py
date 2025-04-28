7c6d4a347e0003ef5e25d7b0f0736ce7f8b637fa
Blinde22 <gucio.miksa@gmail.com>
2025-04-28 20:04:35 +0200

    Update tax.py

--- app/models/tax.py
+++ app/models/tax.py
@@ -5,6 +5,6 @@ class Tax(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(150), nullable=False)
-    due_day = db.Column(db.Integer, nullable=False)  # e.g., 20th of the month
+    due_day = db.Column(db.Integer, nullable=False)  
     last_paid = db.Column(db.Date, nullable=True)
     is_paid = db.Column(db.Boolean, default=False)
