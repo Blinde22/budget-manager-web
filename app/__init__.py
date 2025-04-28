9e2a8d06eddff07558995a62a8da0609a489c9a9
Blinde22 <gucio.miksa@gmail.com>
2025-04-28 20:43:33 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from app.models.tax import Tax
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-
     
     app.config['SECRET_KEY'] = 'ultrasecretkey1312'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -23,9 +21,16 @@ def create_app():
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-
+    
     from app.routes import main
     from app.auth.routes import auth
+
+   
+    app.register_blueprint(main)
+    app.register_blueprint(auth)
+
+    return app
+
 
     app.register_blueprint(main)
     app.register_blueprint(auth)
