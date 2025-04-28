08da33d1d7a93c60ddc244142e3cebc716c48c08
Blinde22 <gucio.miksa@gmail.com>
2025-04-28 20:13:14 +0200

    Update tax.py

--- app/models/tax.py
+++ app/models/tax.py
@@ -1,10 +1,12 @@
-from app import db
-from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class Tax(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(150), nullable=False)
-    due_day = db.Column(db.Integer, nullable=False)  
+    due_day = db.Column(db.Integer, nullable=False)
     last_paid = db.Column(db.Date, nullable=True)
     is_paid = db.Column(db.Boolean, default=False)
+
